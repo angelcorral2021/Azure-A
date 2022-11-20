ec4bce78dcb55f20ab2b364b9ff507c64b6a8099
Angel Corral <82479794+angelcorral2021@users.noreply.github.com>
2022-11-20 12:07:41 -0300

    Update index.php
    PRUEBA DE DESPLIEGUE

--- Application/index.php
+++ Application/index.php
@@ -1,67 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-	<meta charset="utf-8">
-	<title>PHP Application</title>
-	<link href="css/site.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/otro.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    
 </head>
-
 <body>
-<div class="main-container">
-        <div class="cloud-image">
-            <img src="img/successCloudNew.svg" alt="successCloudNew" />
-        </div>
-        <div class="content">
-            <div class="tweet-container">
-            <a href="http://twitter.com/intent/tweet/?text=I%20just%20created%20a%20new%20PHP%20website%20on%20Azure%20using%20Github%20Actions&hashtags=GithubActions%20%40Azure">
-                <img src="img/tweetThis.svg" alt="tweetThis" />
-            </a>
-        </div>
-            <div class="content-body">
-                <div class="success-text">Success!</div>
-                <div class="description line-1">
-				<?php
-				echo "Your Github Repository with Github Actions has been successfully setup";
-				?>
-				</div>
-                <div class="description line-2">
-				<?php
-				$appType = "PHP";
-				echo "Your $appType app is up and running on Azure";
-				?>
-				</div>
-                <div class="next-steps-container">
-                    <div class="next-steps-header">Next up</div>
-                    <div class="next-steps-body">
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/cloneWhite.svg" alt="cloneWhite" >
-                            </div>
-                            <div class="step-text"><a href="https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository">Clone your code repository and start developing your application on IDE of your choice</a></div>
-                        </div>
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/deployWhite.svg" alt="deployWhite">
-                            </div>
-                            <div class="step-text"><a href="https://docs.github.com/en/actions">View your CI/CD pipeline on Github and customize it as per your needs</a></div>
-                        </div>
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/stackWhite.svg" alt="stackWhite">
-                            </div>
-                            <div class="step-text"><a href="http://portal.azure.com">View your service stack in the Azure Portal</a></div>
-                        </div>
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/lightbulbWhite.svg" alt="lightbulbWhite">
-                            </div>
-                            <div class="step-text"><a href="https://docs.github.com">Learn more about all you can do with Github by visiting the documentation</a></div>
-                        </div>
-                    </div>
+<?php
+    include("funciones.php");
+    error_reporting(0);
+    if ($_POST['btnlog'] == "Ingresar") {
+        if ($_POST['txtusu'] == "admin" && $_POST['txtpas'] == "123") { 
+                echo "<script>window.location ='menu.php'</script>";
+        } else {
+            echo "<script>alert('Datos ingresados incorrectos')</script>";
+        }
+    }
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-lg-4 col-xl-4 col-sm-4"></div>
+            <div class="col-md-4  col-lg-4 col-xl-4 col-sm-4 text-center">
+                <div class="col-md-2 col-lg-2 col-xl-2 col-sm-2">
                 </div>
+                <div class="col-md-10  col-lg-10 col-xl-10 text-center">
+                    <h2>Bodegas Don Gato</h2>
+                    <i class="fas fa-cat" style="font-size: 4rem; ;"></i>
+                    <form action="" method="POST" class="text-center">
+                        <div class="mb-3 pt-3">
+                            <input type="text" class="form-control" id="usuario" placeholder="Usuario" name="txtusu">
+                        </div>
+                        <div class="mb-3 ">
+                            <input type="text" class="form-control" id="usuario" placeholder="Contraseña" name="txtpas">
+                        </div>
+    
+                        <div class="mb-3  text-center">
+                            <button class="btn btn-dark bg-dark " name="btnlog" value="Ingresar">Ingresar</button>
+                        </div>
+                        <div class="mb-3  text-center">
+                            <a href="" class="text-dark">Recuperar Contraseña</a>
+                        </div>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 col-sm-2">
+                </div>
+                </form>
             </div>
+            <div class="col-md-4 col-lg-4 col-xl-4 col-sm-4"></div>
         </div>
     </div>
+    
 </body>
 </html>
